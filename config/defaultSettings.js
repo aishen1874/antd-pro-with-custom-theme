c70e9199130c1d8ad1f139d49e44dc0a10891fbc
qinhui <932433511@qq.com>
2019-12-16 18:35:22 +0800

    添加添加主题配置的一些基本页面

--- config/defaultSettings.js
+++ config/defaultSettings.js
@@ -1,17 +1,35 @@
 export default {
-  navTheme: 'dark',
-  // 拂晓蓝
+  navTheme: 'light',
   primaryColor: 'daybreak',
-  layout: 'sidemenu',
-  contentWidth: 'Fluid',
+  layout: 'topmenu',
+  contentWidth: 'Fixed',
   fixedHeader: false,
   autoHideHeader: false,
   fixSiderbar: false,
-  colorWeak: false,
   menu: {
     locale: true,
   },
   title: 'Ant Design Pro',
   pwa: false,
   iconfontUrl: '',
+  collapse: true,
+  language: 'zh-CN',
 };
+
+// {
+//   navTheme: 'dark',
+//   // 拂晓蓝
+//   primaryColor: 'daybreak',
+//   layout: 'sidemenu',
+//   contentWidth: 'Fluid',
+//   fixedHeader: false,
+//   autoHideHeader: false,
+//   fixSiderbar: false,
+//   colorWeak: false,
+//   menu: {
+//     locale: true,
+//   },
+//   title: 'Ant Design Pro',
+//   pwa: false,
+//   iconfontUrl: '',
+// }
